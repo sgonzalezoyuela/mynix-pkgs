28414cdc839f0dd61308d49602d38ed463f5d754
sgonzalez <sgonzalezoyuela@gmail.com>
2023-08-19 20:13:48 -0300

    Initial import

---
+++ jossoctl/flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A Go application";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        jossoctl.${system} = with nixpkgs.legacyPackages.${system}; stdenv.mkDerivation rec {
+          pname = "jossoctl-go";
+          version = "v0.5.5";
+
+          src = builtins.fetchTarball {
+            url = "https://github.com/atricore/jossoctl-go/archive/refs/tags/v0.5.5.tar.gz";
+            sha256 = "12jcqmhlp2g2xpwprfrmarycpksxw9i7xh89i7b6mjkhvbwd10z8";
+          };
+
+          buildInputs = [ go ];
+
+          buildPhase = ''
+            export GOCACHE=$(mktemp -d)
+            go build -o ./jossoctl-go ./jossoctl
+          '';
+
+          installPhase = ''
+            install -D ./jossoctl-go $out/bin/jossoctl
+          '';
+
+          meta = with lib; {
+            description = "JOSSO command line tool";
+            license = licenses.mit; # Update as needed
+          };
+        };
+      in {
+        packages = jossoctl;
+        defaultPackage = jossoctl.${system};
+      });
+}
